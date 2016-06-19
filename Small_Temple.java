cbc260682f01f35bd02dda57d47c12b63a963cc5
HwangEoJun <eojun23@gmail.com>
2016-06-20 01:11:58 +0900

    change add way like a Temple class..

--- Small_Temple.java
+++ Small_Temple.java
@@ -7,15 +7,15 @@ public class Small_Temple extends Temple{
 	
 	//Overriding
 	public void buy(Player player){
-		
 		player.money -= 30;
-		Small_Temple[] tmp = new Small_Temple[player.small_temples.length+1];            // Makes the length of small_temples more longer            
-		for(int i=0; i<player.small_temples.length; i++){ 				 // Copy the element
-			tmp[i] = player.small_temples[i];
+		
+		for(int i=0; i<player.small_temples.length; i++){
+			if(player.small_temples[i] == null){		              //If element is empty,
+				player.small_temples[i] = new Small_Temple(name);     //then add a element
+				player.small_temples[i].zen = 3;	              //small_temple's zen is 3
+				break;
+			}
 		}
-		player.small_temples = tmp;							 // Change small_temples to tmp
-		player.small_temples[player.small_temples.length-1] = new Small_Temple(name);	 // Add new element at last index
-		player.small_temples[player.small_temples.length-1].zen = 3;			 // Small_Temple's zen is 3
 		
 	}
 }
