de5b94adae0e4e0be7c46be8307415ea47c0d8bb
HwangEoJun <eojun23@gmail.com>
2016-06-20 01:10:26 +0900

    I change add way.
    I think the previous one is nasty.
    Previous one make new array and copy continuously.
    But this one make array once.

--- Temple.java
+++ Temple.java
@@ -10,15 +10,15 @@ public class Temple {
 	}
 	
 	public void buy (Player player){
-		
 		player.money -= 70;
-		Temple[] tmp = new Temple[player.temples.length+1];           // Makes the length of temples more longer
-		for(int i=0; i<player.temples.length; i++){                   // Copy the element
-			tmp[i] = player.temples[i];
+		
+		for(int i=0; i<player.temples.length; i++){
+			if(player.temples[i] == null){				// If element is empty,
+				player.temples[i] = new Temple(name);           // then add a element
+				player.temples[i].zen = 5;		        // temple's zen is 5
+				player.temples[i].money = 15;			// temple's money is 15
+				break;
+			}
 		}
-		player.temples = tmp;					      // Change temples to tmp
-		player.temples[player.temples.length-1] = new Temple(name);   // Add new element at last index
-		player.temples[player.temples.length-1].money = 15;           // Temple's money is $15
-		player.temples[player.temples.length-1].zen = 5;              // Temple's zen is 5
 	}
 }
